8fb434bc460f6730fb595f01c6672b95bb5d3dd6
Tony Cherevko <anton.ch.tr@gmail.com>
2022-04-05 09:03:56 +0700

    Semaphores

---
+++ GCD/Semaphores.playground/Contents.swift
@@ -0,0 +1,33 @@
+import UIKit
+import PlaygroundSupport
+
+PlaygroundPage.current.needsIndefiniteExecution = true
+
+let queue = DispatchQueue(label: "Tony", attributes: .concurrent)
+let semaphore = DispatchSemaphore(value: 2)
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    Thread.sleep(forTimeInterval: 4)
+    print("Block 1")
+    semaphore.signal()
+}
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    Thread.sleep(forTimeInterval: 2)
+    print("Block 2")
+    semaphore.signal()
+}
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    print("Block 3")
+    semaphore.signal()
+}
+
+queue.async {
+    semaphore.wait(timeout: .distantFuture)
+    print("Block 4")
+    semaphore.signal()
+}
