1b53d4dfd5531c94bc9537c4e01b72fd8923c214
Tony Cherevko <anton.ch.tr@gmail.com>
2022-04-05 07:41:38 +0700

    GCD
    initial GCD try

---
+++ GCD/SecondVC.swift
@@ -0,0 +1,47 @@
+//
+//  SecondVC.swift
+//  GCD
+//
+//  Created by Tony on 05.04.2022.
+//
+
+import UIKit
+
+class SecondVC: UIViewController {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    
+    fileprivate var imageURL: URL?
+    fileprivate var image: UIImage? {
+        get {
+            return imageView.image
+        } set {
+            activityIndicator.startAnimating()
+            activityIndicator.isHidden = true
+            imageView.image = newValue
+            imageView.sizeToFit()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        fetchImage()
+    }
+    
+    fileprivate func fetchImage() {
+        imageURL = URL(string: "https://upload.wikimedia.org/wikipedia/commons/0/07/Huge_ball_at_Vilnius_center.jpg")
+        activityIndicator.isHidden = false
+        activityIndicator.startAnimating()
+        let queue = DispatchQueue.global(qos: .utility)
+        
+        queue.async {
+            guard let url = self.imageURL, let imageData = try? Data(contentsOf: url) else { return }
+            DispatchQueue.main.async {
+                self.image = UIImage(data: imageData)
+            }
+        }
+        
+    }
+}
